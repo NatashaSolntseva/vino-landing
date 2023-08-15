6d6a000974e0213f049b7c176da4c3c006e23551
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2023-08-15 22:48:35 +0300

    feat: section slider footer add

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
-
+window.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 1.4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    initialSlide: 1,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    parallax: true,
+  });
+});
