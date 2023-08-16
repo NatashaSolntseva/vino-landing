0e93c8151be1f920ed788a002a3fb13b3c932734
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2023-08-16 18:19:48 +0300

    feat: add media

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,39 @@
 window.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".swiper", {
     loop: true,
+    initialSlide: 1,
+    parallax: true,
     slidesPerView: 1.4,
     centeredSlides: true,
     spaceBetween: 30,
-    initialSlide: 1,
     autoplay: {
       delay: 2000,
-      disableOnInteraction: false,
     },
-    parallax: true,
     breakpoints: {
-      576: {},
-      768: {},
-      1024: {},
+      576: {
+        slidesPerView: 1.1,
+        centeredSlides: true,
+        spaceBetween: 20,
+        autoplay: {
+          delay: 2000,
+        },
+      },
+      768: {
+        slidesPerView: 1.4,
+        centeredSlides: true,
+        spaceBetween: 30,
+        autoplay: {
+          delay: 2000,
+        },
+      },
+      1024: {
+        slidesPerView: 1.4,
+        centeredSlides: true,
+        spaceBetween: 30,
+        autoplay: {
+          delay: 2000,
+        },
+      },
       1920: {
         spaceBetween: 50,
         slidesPerView: 3,
